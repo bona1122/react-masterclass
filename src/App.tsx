6e931ec771d758039fc5aa0a52741ce8de1f605f
bona1122 <vnclfjqm12@gmail.com>
2024-02-08 16:06:54 +0900

    styled-components basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: teal;
+`;
+const BoxTwo = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: tomato;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
 }
 
 export default App;
