e95b1cad201aacacfce20eb43b7581f6988b26dd
bona1122 <vnclfjqm12@gmail.com>
2024-02-12 18:00:05 +0900

    React Hook Form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,26 +1,49 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [todo, setTodo] = useState("");
+//   const [todoError, setTodoError] = useState("");
+//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = e;
+//     setTodoError("");
+//     setTodo(value);
+//   };
+//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if (todo.length < 10) return setTodoError("to do should be longer");
+//     console.log("submit");
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           value={todo}
+//           onChange={onChange}
+//           type="text"
+//           placeholder="Write a to do"
+//         />
+//         <button>Add</button>
+//         {todoError !== "" ? todoError : null}
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [todo, setTodo] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setTodo(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(todo);
-  };
+  const { register, watch } = useForm();
+  // console.log(register("todo"));
+  console.log(watch()); // 입력갑 추적 가능
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={todo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write a to do"
-        />
+      <form>
+        <input {...register("email")} placeholder="Write a to do" />
+        <input {...register("firstName")} placeholder="Write a to do" />
+        <input {...register("lastName")} placeholder="Write a to do" />
+        <input {...register("username")} placeholder="Write a to do" />
+        <input {...register("password")} placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
