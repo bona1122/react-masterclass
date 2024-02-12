d8343267c9c54d4dbab25bcd55a517fa0f63ba57
bona1122 <vnclfjqm12@gmail.com>
2024-02-12 18:59:14 +0900

    More React Hook Form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -32,19 +32,102 @@ import { useForm } from "react-hook-form";
 //   );
 // }
 
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
 function ToDoList() {
-  const { register, watch } = useForm();
-  // console.log(register("todo"));
-  console.log(watch()); // 입력갑 추적 가능
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        // 특정항목에 에러 발생시키기
+        "password1",
+        { message: "password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraError", { message: "Server offline." }); //특정항목이 아닌 전체 form 에 해당되는 에러
+  };
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Write a to do" />
-        <input {...register("firstName")} placeholder="Write a to do" />
-        <input {...register("lastName")} placeholder="Write a to do" />
-        <input {...register("username")} placeholder="Write a to do" />
-        <input {...register("password")} placeholder="Write a to do" />
+      <form
+        style={{ display: "flex", flexDirection: "column", maxWidth: "300px" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "only naver!",
+            },
+          })}
+          placeholder="email"
+        />
+        <span>{errors?.email?.message?.toString()}</span>
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nico allowed" : true,
+              noLee: (value) =>
+                value.includes("Lee") ? "no Lee allowed" : true,
+            },
+          })}
+          placeholder="firstname"
+        />
+        <span>{errors?.firstName?.message?.toString()}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="lastname"
+        />
+        <span>{errors?.lastName?.message?.toString()}</span>
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="username"
+        />
+        <span>{errors?.username?.message?.toString()}</span>
+        <input
+          {...register("password", {
+            required: "password is required",
+            minLength: {
+              value: 5,
+              message: "your password is too short",
+            },
+          })}
+          placeholder="password"
+        />
+        <span>{errors?.password?.message?.toString()}</span>
+        <input
+          {...register("password1", {
+            required: "password1 is required",
+            minLength: {
+              value: 5,
+              message: "your password1 is too short",
+            },
+          })}
+          placeholder="password1"
+        />
+        <span>{errors?.password1?.message?.toString()}</span>
+
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
