c2dba1f2a65718fe871706b8d723540889a14eb0
bona1122 <vnclfjqm12@gmail.com>
2024-02-17 23:36:23 +0900

    selector get function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-function App(){
+import { useRecoilState } from "recoil";
+import { hourSelector, minutesState } from "./atoms";
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
   return (
     <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      ></input>
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      ></input>
     </div>
-  )
+  );
 }
 export default App;
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,18 @@
 import { atom, selector } from "recoil";
+
+export const minutesState = atom({
+  key: "minutesState",
+  default: 0,
+});
+
+export const hourSelector = selector<number>({
+  key: "hourSelector",
+  get: ({ get }) => {
+    const minutes = get(minutesState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minutesState, minutes);
+  },
+});
